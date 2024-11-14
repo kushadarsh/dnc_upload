e72550d9e4708cb800b2d6c7c880b86b63d14979
kushadarsh <adarshkush1311@gmail.com>
2024-11-15 01:07:34 +0530

    route changes commit

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server';
 
 // Configure AWS SDK
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.NEXT_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.NEXT_AWS_SECRET_ACCESS_KEY,
+  region: process.env.NEXT_AWS_REGION,
 });
 
 export async function POST(req) {
